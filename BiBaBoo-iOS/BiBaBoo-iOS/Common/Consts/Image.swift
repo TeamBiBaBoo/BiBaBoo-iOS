58168ad603a0bb33b6bb67bebb5c498e1d1282f9
정채은 <cce523@hanmail.net>
2023-04-05 16:21:38 +0900

    Add [#3] image 및 icon 선언

--- BiBaBoo-iOS/BiBaBoo-iOS/Common/Consts/Image.swift
+++ BiBaBoo-iOS/BiBaBoo-iOS/Common/Consts/Image.swift
@@ -11,9 +11,40 @@ extension Const {
     struct Image {
         
         // MARK: icon
+        static let checkNo = UIImage(named: "check=no")
+        static let checkYes = UIImage(named: "check=yes")
+        static let icnEtc = UIImage(named:"etc")
+        static let icnFood = UIImage(named:"food")
+        static let icnGrowth = UIImage(named:"growth")
+        static let icnAddFrieds = UIImage(named:"icn_addFriends")
+        static let icnAddList = UIImage(named:"icn_addList")
+        static let icnCircleX = UIImage(named: "icn_circlex")
+        static let icnDelete = UIImage(named: "icn_delete")
+        static let icnDone = UIImage(named: "icn_done")
+        static let icnExit = UIImage(named: "icn_exit")
+        static let icnPic = UIImage(named: "icn_pic")
+        static let icnSticker = UIImage(named: "icn_sticker")
+        static let icnX = UIImage(named: "icn_x")
+        static let icnBack = UIImage(named: "icn_back")
+        static let icnMovie = UIImage(named: "movie")
+        static let icnShopping = UIImage(named: "shopping")
+        static let icnSetting = UIImage(named: "setting")
+        static let icnTravel = UIImage(named: "travel")
+        
+        static let homeDefault = UIImage(named: "status=default")
+        static let twinkleDefault = UIImage(named: "status=default-1")
+        static let profileDefault = UIImage(named: "status=default-2")
+        static let friendDefault = UIImage(named: "status=default-3")
+        static let notiDefault = UIImage(named: "status=default-4")
+        static let homeSelected = UIImage(named: "status=selected")
+        static let twinkleSelected = UIImage(named: "status=selected-1")
+        static let profileSelected = UIImage(named: "status=selected-2")
+        static let friendSelected = UIImage(named: "status=selected-3")
+        static let notiSelected = UIImage(named: "status=selected-4")
         
         
         // MARK: image
+        static let addButton = UIImage(named: "btn_addList")
         static let sticker1 = UIImage(named: "sticker_1")
         
     }
