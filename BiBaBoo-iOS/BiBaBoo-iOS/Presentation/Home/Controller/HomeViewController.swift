b875a3a54ebdba8094069ea8fa9623e943dc2c59
정채은 <cce523@hanmail.net>
2023-04-05 16:03:17 +0900

    Setting [#3] file 세팅

---
+++ BiBaBoo-iOS/BiBaBoo-iOS/Presentation/Home/Controller/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  HomeViewController.swift
+//  BiBaBoo-iOS
+//
+//  Created by 정채은 on 2023/04/05.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
---
+++ BiBaBoo-iOS/BiBaBoo-iOS/Presentation/Home/View/HomeTopBarView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeTopBarView.swift
+//  BiBaBoo-iOS
+//
+//  Created by 정채은 on 2023/04/05.
+//
+
+import UIKit
+
+import Then
+import SnapKit
+
+class HomeTopBarView: UIView {
+    
+    private let stickerImageView: UIImageView = UIImageView().then {_ in
+        $0.image = Const.Image.
+    }
+
+
+}
