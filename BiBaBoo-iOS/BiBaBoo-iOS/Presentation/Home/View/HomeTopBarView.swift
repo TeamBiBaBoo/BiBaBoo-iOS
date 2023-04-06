a7747baab9e78f897635f0b98d195d48e433af2d
정채은 <cce523@hanmail.net>
2023-04-06 14:18:06 +0900

    Fix [#3] HomeTopBarView background 색상 수정

--- BiBaBoo-iOS/BiBaBoo-iOS/Presentation/Home/View/HomeTopBarView.swift
+++ BiBaBoo-iOS/BiBaBoo-iOS/Presentation/Home/View/HomeTopBarView.swift
@@ -30,12 +30,17 @@ class HomeTopBarView: UIView {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
+        style()
         setUI()
 
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func style() {
+        self.backgroundColor = .white
     }
     
     private func setUI() {
